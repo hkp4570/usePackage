49ed83d3925b8e56061e47921237d5261ca73c61
hkp <sxhkp163@163.com>
2021-11-19 19:06:44 +0800

    测试antv6  api

--- src/pages/g6/index.jsx
+++ src/pages/g6/index.jsx
@@ -1,198 +1,185 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-// import {data} from './data';
 import G6 from '@antv/g6';
+//codesandbox.io/s/elastic-leftpad-vni9s?file=/index.js
 
-const data = {
+https: const data = {
     nodes: [
         {
-            id: '1',
-            dataType: 'alps',
-            name: 'alps_file1',
-            conf: [
-                {
-                    label: 'conf',
-                    value: 'pai_graph.conf',
-                },
-                {
-                    label: 'dot',
-                    value: 'pai_graph.dot',
-                },
-                {
-                    label: 'init',
-                    value: 'init.rc',
-                },
-            ],
+            status: 0, //节点状态 用于更改颜色
+            id: '5fd5ef0f', //节点id
+            label: '是否报警', //节点名称
+            shape: 'flow-rhombus', //节点形状类型
+            size: '80*72', //节点大小
+            type: 'node', //节点类型
+            x: 463.515625,
+            y: 367, //节点位于画布位置
         },
         {
-            id: '2',
-            dataType: 'alps',
-            name: 'alps_file2',
-            conf: [
-                {
-                    label: 'conf',
-                    value: 'pai_graph.conf',
-                },
-                {
-                    label: 'dot',
-                    value: 'pai_graph.dot',
-                },
-                {
-                    label: 'init',
-                    value: 'init.rc',
-                },
-            ],
+            // status: 0,
+            id: 'b5d33740',
+            label: '属实',
+            // shape: 'flow-rect',
+            // size: '80*48',
+            // type: 'node',
+            // x: 219.015625,
+            // y: 483.5,
         },
         {
-            id: '3',
-            dataType: 'alps',
-            name: 'alps_file3',
-            conf: [
-                {
-                    label: 'conf',
-                    value: 'pai_graph.conf',
-                },
-                {
-                    label: 'dot',
-                    value: 'pai_graph.dot',
-                },
-                {
-                    label: 'init',
-                    value: 'init.rc',
-                },
-            ],
+            // status: 0,
+            id: '8402b23d',
+            label: '人工核实',
+            // shape: 'flow-rhombus',
+            // size: '80*72',
+            // type: 'node',
+            // x: 219.515625,
+            // y: 359,
         },
         {
-            id: '4',
-            dataType: 'sql',
-            name: 'sql_file1',
-            conf: [
-                {
-                    label: 'conf',
-                    value: 'pai_graph.conf',
-                },
-                {
-                    label: 'dot',
-                    value: 'pai_graph.dot',
-                },
-                {
-                    label: 'init',
-                    value: 'init.rc',
-                },
-            ],
+            // status: 0,
+            id: '746dd7be',
+            label: '报警',
+            // shape: 'flow-rect',
+            // size: '80*48',
+            // type: 'node',
+            // x: 464.015625,
+            // y: 489.5,
         },
         {
-            id: '5',
-            dataType: 'sql',
-            name: 'sql_file2',
-            conf: [
-                {
-                    label: 'conf',
-                    value: 'pai_graph.conf',
-                },
-                {
-                    label: 'dot',
-                    value: 'pai_graph.dot',
-                },
-                {
-                    label: 'init',
-                    value: 'init.rc',
-                },
-            ],
+            // status: 0,
+            id: '8891eaf9',
+            label: '启动',
+            // shape: 'flow-circle',
+            // size: '72*72',
+            // type: 'node',
+            // x: 457.015625,
+            // y: 117.5,
         },
         {
-            id: '6',
-            dataType: 'feature_etl',
-            name: 'feature_etl_1',
-            conf: [
-                {
-                    label: 'conf',
-                    value: 'pai_graph.conf',
-                },
-                {
-                    label: 'dot',
-                    value: 'pai_graph.dot',
-                },
-                {
-                    label: 'init',
-                    value: 'init.rc',
-                },
-            ],
+            // status: 0,
+            id: '48f92c7c',
+            label: '触发火灾',
+            // shape: 'flow-rect',
+            // size: '80*48',
+            // type: 'node',
+            // x: 460.015625,
+            // y: 220,
         },
         {
-            id: '7',
-            dataType: 'feature_etl',
-            name: 'feature_etl_1',
-            conf: [
-                {
-                    label: 'conf',
-                    value: 'pai_graph.conf',
-                },
-                {
-                    label: 'dot',
-                    value: 'pai_graph.dot',
-                },
-                {
-                    label: 'init',
-                    value: 'init.rc',
-                },
-            ],
+            // status: 0,
+            id: '0f2d9c3d',
+            label: '119',
+            // shape: 'flow-rect',
+            // size: '80*48',
+            // type: 'node',
+            // x: 325.515625,
+            // y: 584,
         },
         {
-            id: '8',
-            dataType: 'feature_extractor',
-            name: 'feature_extractor',
-            conf: [
-                {
-                    label: 'conf',
-                    value: 'pai_graph.conf',
-                },
-                {
-                    label: 'dot',
-                    value: 'pai_graph.dot',
-                },
-                {
-                    label: 'init',
-                    value: 'init.rc',
-                },
-            ],
+            // status: 0,
+            id: '820ef87e',
+            label: '120',
+            // shape: 'flow-rect',
+            // size: '80*48',
+            // type: 'node',
+            // x: 584.515625,
+            // y: 586,
+        },
+        {
+            // status: 0,
+            id: '18b36170',
+            label: '事件结束',
+            // shape: 'flow-capsule',
+            // size: '80*48',
+            // type: 'node',
+            // x: 463.515625,
+            // y: 665,
         },
     ],
     edges: [
         {
-            source: '1',
-            target: '2',
+            // id: 'e0f5a4a8', //连接线id
+            // status: 0, //连接线状态 用于更改颜色
+            source: '48f92c7c', //连接线上游节点id
+            // sourceAnchor: 2, //上游节点连线位置 每个节点有四个连接点顺时针对应 0、1、2、3
+            target: '5fd5ef0f', //连接线目标节点id
+            // targetAnchor: 0, //上游节点连线位置
         },
         {
-            source: '1',
-            target: '3',
+            // id: '3f96e12c',
+            // status: 0,
+            source: '48f92c7c',
+            // sourceAnchor: 3,
+            target: '8402b23d',
+            // targetAnchor: 0,
         },
         {
-            source: '2',
-            target: '4',
+            // id: 'be492bff',
+            // status: 0,
+            source: '8402b23d',
+            // sourceAnchor: 2,
+            target: 'b5d33740',
+            // targetAnchor: 0,
         },
         {
-            source: '3',
-            target: '4',
+            // id: '812cf224',
+            // status: 0,
+            source: 'b5d33740',
+            // sourceAnchor: 1,
+            target: '746dd7be',
+            // targetAnchor: 3,
         },
         {
-            source: '4',
-            target: '5',
+            // id: '9ca6b433',
+            // status: 0,
+            source: '5fd5ef0f',
+            // sourceAnchor: 2,
+            target: '746dd7be',
+            // targetAnchor: 0,
         },
         {
-            source: '5',
-            target: '6',
+            // id: 'eac39a7e',
+            // status: 0,
+            source: '8891eaf9',
+            // sourceAnchor: 2,
+            target: '48f92c7c',
+            // targetAnchor: 0,
         },
         {
-            source: '6',
-            target: '7',
+            // id: 'eed085a7',
+            // status: 0,
+            source: '746dd7be',
+            // sourceAnchor: 2,
+            target: '0f2d9c3d',
+            // targetAnchor: 0,
         },
         {
-            source: '6',
-            target: '8',
+            // id: '0d938e74',
+            // status: 0,
+            source: '746dd7be',
+            // sourceAnchor: 2,
+            target: '820ef87e',
+            // targetAnchor: 0,
+        },
+        {
+            // id: 'c9bd0003',
+            // status: 0,
+            source: '0f2d9c3d',
+            // sourceAnchor: 2,
+            target: '18b36170',
+            // targetAnchor: 0,
+        },
+        {
+            // id: '9c477a6a',
+            // status: 0,
+            source: '820ef87e',
+            // sourceAnchor: 2,
+            target: '18b36170',
+            // targetAnchor: 0,
         },
     ],
 };
+
 const Index = () => {
     const ref = React.useRef(null);
     let graph = null;
@@ -201,17 +188,12 @@ const Index = () => {
             graph = new G6.Graph({
                 container: ReactDOM.findDOMNode(ref.current),
                 width: 1200,
-                height: 800,
+                height: 1200,
+                // 'random' | 'radial' | 'mds' | 'circular' | 'fruchterman' | 'force' | 'gForce' | 'dagre' |
+                // 'concentric' | 'grid' | 'forceAtlas2'
                 layout: {
                     type: 'dagre',
-                    nodesepFunc: (d) => {
-                        if (d.id === '3') {
-                            return 500;
-                        }
-                        return 50;
-                    },
-                    ranksep: 70,
-                    controlPoints: true,
+                    nodesep: 10,
                 },
             });
         }
