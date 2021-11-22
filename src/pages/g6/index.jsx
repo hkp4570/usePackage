903f8262acf12b03a749279e58abf6eecc5fdd3a
hkp <sxhkp163@163.com>
2021-11-22 19:17:52 +0800

    g6v4.4.1节点和边基本配置

--- src/pages/g6/index.jsx
+++ src/pages/g6/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import G6 from '@antv/g6';
-//codesandbox.io/s/elastic-leftpad-vni9s?file=/index.js
+//codesandbox.io/s/beautiful-driscoll-j1gom?file=/index.js
 
 https: const data = {
     nodes: [
@@ -9,173 +9,304 @@ https: const data = {
             status: 0, //节点状态 用于更改颜色
             id: '5fd5ef0f', //节点id
             label: '是否报警', //节点名称
-            shape: 'flow-rhombus', //节点形状类型
-            size: '80*72', //节点大小
-            type: 'node', //节点类型
-            x: 463.515625,
+            type: 'diamond', //节点形状类型
+            size: [80, 72], //节点大小
+            // type: "node", //节点类型
+            x: 463.515625, //
             y: 367, //节点位于画布位置
+            style: {
+                fill: '#E2FFF7',
+            },
         },
         {
-            // status: 0,
+            status: 0,
             id: 'b5d33740',
             label: '属实',
-            // shape: 'flow-rect',
-            // size: '80*48',
-            // type: 'node',
-            // x: 219.015625,
-            // y: 483.5,
+            anchorPoints: [
+                [0.5, 0],
+                [1, 0.5],
+            ],
+            type: 'rect',
+            size: [80, 48],
+            x: 219.015625,
+            y: 483.5,
+            style: {
+                fill: '#E7F8FA',
+            },
         },
         {
-            // status: 0,
+            status: 0,
             id: '8402b23d',
             label: '人工核实',
-            // shape: 'flow-rhombus',
-            // size: '80*72',
-            // type: 'node',
-            // x: 219.515625,
-            // y: 359,
+            type: 'diamond',
+            anchorPoints: [
+                [0.5, 0],
+                [0.5, 1],
+            ],
+            size: [80, 72],
+            x: 220.515625,
+            y: 365,
+            style: {
+                fill: '#E2FFF7',
+            },
         },
         {
-            // status: 0,
+            status: 0,
             id: '746dd7be',
             label: '报警',
-            // shape: 'flow-rect',
-            // size: '80*48',
-            // type: 'node',
-            // x: 464.015625,
-            // y: 489.5,
+            type: 'rect',
+            anchorPoints: [
+                [0, 0.5],
+                [0.5, 1],
+                [0.5, 0],
+            ],
+            size: [80, 48],
+            x: 464.015625,
+            y: 489.5,
+            style: {
+                fill: '#E7F8FA',
+            },
         },
         {
-            // status: 0,
+            status: 0,
             id: '8891eaf9',
+            anchorPoints: [[0.5, 1]],
             label: '启动',
-            // shape: 'flow-circle',
-            // size: '72*72',
-            // type: 'node',
-            // x: 457.015625,
-            // y: 117.5,
+            type: 'circle',
+            size: [72, 72],
+            x: 457.015625,
+            y: 117.5,
+            style: {
+                fill: '#FEF7E7',
+            },
         },
         {
-            // status: 0,
+            status: 0,
             id: '48f92c7c',
             label: '触发火灾',
-            // shape: 'flow-rect',
-            // size: '80*48',
-            // type: 'node',
-            // x: 460.015625,
-            // y: 220,
+            type: 'rect',
+            size: [80, 48],
+            x: 460.015625,
+            y: 220,
+            anchorPoints: [
+                [0.5, 0],
+                [0, 0.5],
+                [0.5, 1],
+            ],
+            style: {
+                fill: '#E7F8FA',
+            },
         },
         {
-            // status: 0,
+            status: 0,
             id: '0f2d9c3d',
             label: '119',
-            // shape: 'flow-rect',
-            // size: '80*48',
-            // type: 'node',
-            // x: 325.515625,
-            // y: 584,
+            type: 'rect',
+            size: [80, 48],
+            anchorPoints: [
+                [0.5, 0],
+                [0.5, 1],
+            ],
+            x: 325.515625,
+            y: 584,
+            style: {
+                fill: '#E7F8FA',
+            },
         },
         {
-            // status: 0,
+            status: 0,
             id: '820ef87e',
             label: '120',
-            // shape: 'flow-rect',
-            // size: '80*48',
-            // type: 'node',
-            // x: 584.515625,
-            // y: 586,
+            type: 'rect',
+            size: [80, 48],
+            anchorPoints: [
+                [0.5, 0],
+                [0.5, 1],
+            ],
+            x: 584.515625,
+            y: 586,
+            style: {
+                fill: '#E7F8FA',
+            },
         },
         {
-            // status: 0,
+            status: 0,
             id: '18b36170',
             label: '事件结束',
-            // shape: 'flow-capsule',
-            // size: '80*48',
-            // type: 'node',
-            // x: 463.515625,
-            // y: 665,
+            type: 'ellipse',
+            size: [80, 48],
+            anchorPoints: [[0.5, 0]],
+            x: 463.515625,
+            y: 665,
+            style: {
+                fill: '#F7F0FF',
+            },
         },
     ],
     edges: [
         {
-            // id: 'e0f5a4a8', //连接线id
-            // status: 0, //连接线状态 用于更改颜色
+            //  触发火灾 -> 是否报警
+            id: 'e0f5a4a8', //连接线id
+            status: 0, //连接线状态 用于更改颜色
             source: '48f92c7c', //连接线上游节点id
-            // sourceAnchor: 2, //上游节点连线位置 每个节点有四个连接点顺时针对应 0、1、2、3
+            sourceAnchor: 2, //上游节点连线位置 每个节点有四个连接点顺时针对应 0、1、2、3
             target: '5fd5ef0f', //连接线目标节点id
-            // targetAnchor: 0, //上游节点连线位置
+            targetAnchor: 0, //上游节点连线位置
+            style: {
+                endArrow: {
+                    path: G6.Arrow.triangle(8, 10, 2),
+                    d: 7,
+                    fill: '#ccc',
+                },
+            },
         },
         {
-            // id: '3f96e12c',
-            // status: 0,
+            //  触发火灾 -> 人工核实
+            id: '3f96e12c',
+            status: 0,
             source: '48f92c7c',
-            // sourceAnchor: 3,
+            type: 'quadratic',
+            curvePosition: 1,
+            controlPoints: [{ x: 215, y: 270 }],
+            sourceAnchor: 3,
             target: '8402b23d',
-            // targetAnchor: 0,
+            targetAnchor: 0,
+            style: {
+                endArrow: {
+                    path: G6.Arrow.triangle(8, 10, 2),
+                    d: 7,
+                    fill: '#ccc',
+                },
+            },
         },
         {
-            // id: 'be492bff',
-            // status: 0,
+            //  人工核实 -> 属实
+            id: 'be492bff',
+            status: 0,
             source: '8402b23d',
-            // sourceAnchor: 2,
+            sourceAnchor: 1,
             target: 'b5d33740',
-            // targetAnchor: 0,
+            // targetAnchor: 1,
+            style: {
+                endArrow: {
+                    path: G6.Arrow.triangle(8, 10, 2),
+                    d: 7,
+                    fill: '#ccc',
+                },
+            },
         },
         {
-            // id: '812cf224',
-            // status: 0,
+            // 属实 -> 报警
+            id: '812cf224',
+            status: 0,
             source: 'b5d33740',
-            // sourceAnchor: 1,
+            sourceAnchor: 1,
             target: '746dd7be',
-            // targetAnchor: 3,
+            targetAnchor: 3,
+            style: {
+                endArrow: {
+                    path: G6.Arrow.triangle(8, 10, 2),
+                    d: 7,
+                    fill: '#ccc',
+                },
+            },
         },
         {
-            // id: '9ca6b433',
-            // status: 0,
+            // 是否报警 -> 报警
+            id: '9ca6b433',
+            status: 0,
             source: '5fd5ef0f',
-            // sourceAnchor: 2,
             target: '746dd7be',
-            // targetAnchor: 0,
+            style: {
+                endArrow: {
+                    path: G6.Arrow.triangle(8, 10, 2),
+                    d: 7,
+                    fill: '#ccc',
+                },
+            },
         },
         {
-            // id: 'eac39a7e',
-            // status: 0,
+            // 启动 -> 触发火灾
+            id: 'eac39a7e',
+            status: 0,
             source: '8891eaf9',
-            // sourceAnchor: 2,
+            sourceAnchor: 0,
             target: '48f92c7c',
-            // targetAnchor: 0,
+            targetAnchor: 0,
+            style: {
+                endArrow: {
+                    path: G6.Arrow.triangle(8, 10, 2),
+                    d: 7,
+                    fill: '#ccc',
+                },
+            },
         },
         {
-            // id: 'eed085a7',
-            // status: 0,
+            // 报警 -> 119
+            id: 'eed085a7',
+            status: 0,
             source: '746dd7be',
-            // sourceAnchor: 2,
+            sourceAnchor: 1,
             target: '0f2d9c3d',
-            // targetAnchor: 0,
+            targetAnchor: 0,
+            type: 'cubic-vertical',
+            style: {
+                endArrow: {
+                    path: G6.Arrow.triangle(8, 10, 2),
+                    d: 7,
+                    fill: '#ccc',
+                },
+            },
         },
         {
-            // id: '0d938e74',
-            // status: 0,
+            //  报警 -> 120
+            id: '0d938e74',
+            status: 0,
             source: '746dd7be',
-            // sourceAnchor: 2,
+            sourceAnchor: 1,
             target: '820ef87e',
-            // targetAnchor: 0,
+            targetAnchor: 0,
+            type: 'cubic-vertical',
+            style: {
+                endArrow: {
+                    path: G6.Arrow.triangle(8, 10, 2),
+                    d: 7,
+                    fill: '#ccc',
+                },
+            },
         },
         {
-            // id: 'c9bd0003',
-            // status: 0,
+            //  119 -> 事件结束
+            id: 'c9bd0003',
+            status: 0,
             source: '0f2d9c3d',
-            // sourceAnchor: 2,
+            sourceAnchor: 2,
             target: '18b36170',
-            // targetAnchor: 0,
+            targetAnchor: 0,
+            type: 'cubic-vertical',
+            style: {
+                endArrow: {
+                    path: G6.Arrow.triangle(8, 10, 2),
+                    d: 7,
+                    fill: '#ccc',
+                },
+            },
         },
         {
-            // id: '9c477a6a',
-            // status: 0,
+            // 120 -> 事件结束
+            id: '9c477a6a',
+            // status: "loop",
             source: '820ef87e',
-            // sourceAnchor: 2,
+            sourceAnchor: 2,
             target: '18b36170',
-            // targetAnchor: 0,
+            type: 'cubic-vertical',
+            style: {
+                endArrow: {
+                    path: G6.Arrow.triangle(8, 10, 2),
+                    d: 7,
+                    fill: '#ccc',
+                },
+            },
         },
     ],
 };
@@ -187,13 +318,11 @@ const Index = () => {
         if (!graph) {
             graph = new G6.Graph({
                 container: ReactDOM.findDOMNode(ref.current),
-                width: 1200,
-                height: 1200,
-                // 'random' | 'radial' | 'mds' | 'circular' | 'fruchterman' | 'force' | 'gForce' | 'dagre' |
-                // 'concentric' | 'grid' | 'forceAtlas2'
-                layout: {
-                    type: 'dagre',
-                    nodesep: 10,
+                width: 800,
+                height: 1000,
+                defaultNode: {
+                    shape: 'circle',
+                    size: [80],
                 },
             });
         }
